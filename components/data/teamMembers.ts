9573d72dcd03c205a2b11957e9ce28dbcd820053
Nziza-Prince <nzizaprince7@gmail.com>
2025-09-15 19:08:50 +0200

    chore:fixed the member list

--- components/data/teamMembers.ts
+++ components/data/teamMembers.ts
@@ -34,8 +34,8 @@ export const coreMembers = [
     },
   },
   {
-    name: "Niyubahwe Annick",
-    role: "Chief Operations Officer",
+    name: "Niyubahwe Anicke",
+    role: "Chief Technical Officer",
     image: "/annicke.jpeg",
     social: {
       email: "niyubahwe.annick975@gmail.com",
